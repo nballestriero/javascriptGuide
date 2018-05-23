da255ff8529d185c05579bfb62a6cedbd99b47c1
nballestriero <n.ballestriero@gmail.com>
2018-05-23 22:16:28 +0200

    backup

---
+++ Angular/AlarmPanelCams/src/app/servers/servers.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was Created!';
+  serverName = 'default';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was Created! Name is:' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
