553bf29e6d60c92ca7b4a96f59fcb09df450b50e
nballestriero <n.ballestriero@gmail.com>
2018-06-06 08:44:29 +0200

    backup

---
+++ ShoppingList/PROJECT/src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient{
+//  public name: string;
+//  public amount: number;
+//  
+//  constructor(name: string, amount: number){
+//    this.name = name;
+//    this.amount = amount;
+//  }
+// THE CODE ABOVE IS IDENTICAL TO THE FOLLOWING CONSTRUCTOR
+  constructor(public name: string, public amount: number){}
+}
